aaeed5d6b4444e2b9d7fb0d22976982f06e5ae34
Vitor-Gauer <vitormatheusgauer223@gmail.com>
2026-03-13 12:31:57 -0300

    Delete src/main/java/projetojava/donodonegocio/security directory

--- src/main/java/projetojava/donodonegocio/security/AuthResponse.java
+++
@@ -1,7 +0,0 @@
-package projetojava.donodonegocio.security;
-
-public record AuthResponse(CustomUserDetails userDetails, String empresaNome, boolean isMasterLogin) {
-    public AuthResponse(CustomUserDetails userDetails, boolean isMasterLogin) {
-        this(userDetails, null, isMasterLogin);
-    }
-}
